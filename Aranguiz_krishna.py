b1aabe080a3db97ba596d30429f96e3c9dbe2f7e
skaas08 <kr.aranguiz@duocuc.cl>
2025-06-27 20:15:36 -0400

    Create Aranguiz_krishna.py

---
+++ Aranguiz_krishna.py
@@ -0,0 +1,9 @@
+def menu():
+    print("***TOTEM AUTOATENCIÓN RESERVA STRIKE***")
+    print("1. Reservar zapatillas")
+    print("2. Buscar zapatillas reservadas")
+    print("3. Ver stock de reservas")
+    print("4. Salir")
+
+reservas = []
+
